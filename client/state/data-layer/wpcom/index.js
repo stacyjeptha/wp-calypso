bb262df5cadfb83bc577ff48fed85753c4f5ebe1
Ben Lowery <ben.lowery@automattic.com>
2017-01-24 15:48:41 -0500

    move to one data layer stream piece

--- client/state/data-layer/wpcom/index.js
+++ client/state/data-layer/wpcom/index.js
@@ -3,11 +3,13 @@
  */
 import { mergeHandlers } from 'state/data-layer/utils';
 import plans from './plans';
+import reader from './reader';
 import sites from './sites';
 import read from './read';
 
 export const handlers = mergeHandlers(
 	plans,
+	reader,
 	sites,
 	read,
 );
