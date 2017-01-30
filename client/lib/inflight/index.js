30d890e720cdd22f018d0366b74ab751f7a4416f
Ben Lowery <ben.lowery@automattic.com>
2017-01-30 11:42:21 -0500

    trial with lib/inflight. do not like.

--- client/lib/inflight/index.js
+++ client/lib/inflight/index.js
@@ -1,16 +1,31 @@
-var inflight = {};
+const inflight = new Set();
 
 // Utility methods to track inflight async requests
 module.exports = {
 	requestInflight: function( requestKey ) {
-		return requestKey in inflight;
+		return inflight.has( requestKey );
 	},
 
 	requestTracker: function( requestKey, callback ) {
-		inflight[ requestKey ] = true;
-		return function( error, data ) {
-			delete inflight[ requestKey ];
-			callback( error, data );
+		inflight.add( requestKey );
+
+		return function( err, data ) {
+			inflight.delete( requestKey );
+			callback( err, data );
 		};
+	},
+
+	trackPromise: function( requestKey, promise ) {
+		inflight.add( requestKey );
+		promise.then(
+			() => {
+				inflight.delete( requestKey );
+			},
+			() => {
+				inflight.delete( requestKey );
+			}
+		);
+		// return the original promise so any subsequent thens don't flow through our handlers
+		return promise;
 	}
 };
