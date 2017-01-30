a74f08e655ec326938e8f32f1147bd8025cc9ea4
Riad Benguella <benguella@gmail.com>
2017-01-30 14:03:58 +0100

    Stats: Add The Last updated time to the StatsModule component

--- client/state/selectors/index.js
+++ client/state/selectors/index.js
@@ -39,6 +39,7 @@ export getReaderTeams from './get-reader-teams';
 export getSharingButtons from './get-sharing-buttons';
 export getSiteIconId from './get-site-icon-id';
 export getSiteIconUrl from './get-site-icon-url';
+export getSiteStatsQueryDate from './get-site-stats-query-date';
 export getTimezonesByContinent from './get-timezones-by-continent';
 export getUpcomingBillingTransactions from './get-upcoming-billing-transactions';
 export hasBrokenSiteUserConnection from './has-broken-site-user-connection';
