ea5bdf4309fec918d2c99d6989118c0eb7b4f3c2
Jake F - @samouri <samouri@users.noreply.github.com>
2017-01-31 13:07:17 -0500

    added a test for if the promise is rejected

--- client/lib/inflight/test/index.js
+++ client/lib/inflight/test/index.js
@@ -12,7 +12,7 @@ const THE_ANSWER = 42;
 
 describe( 'inflight', ( ) => {
 	describe( 'promiseTracker', () => {
-		it( 'should mark an item as inflight while the request is ongoing', () => {
+		it( 'should mark an item as inflight while the request is ongoing and request succeeds', () => {
 			const key = 'key1';
 			const trackedPromise = inflight.promiseTracker( key, Promise.resolve( THE_ANSWER ) );
 
@@ -22,8 +22,18 @@ describe( 'inflight', ( ) => {
 			} );
 		} );
 
-		it( 'should return the same data as the original promise', () => {
+		it( 'should mark an item as inflight while the request is ongoing and request fails', () => {
 			const key = 'key2';
+			const trackedPromise = inflight.promiseTracker( key, Promise.reject( THE_ANSWER ) );
+
+			expect( inflight.requestInflight( key ) ).to.be.true;
+			return trackedPromise.catch( () => {
+				expect( inflight.requestInflight( key ) ).to.be.false;
+			} );
+		} );
+
+		it( 'should return the same data as the original promise', () => {
+			const key = 'key3';
 			const trackedPromise = inflight.promiseTracker( key, Promise.resolve( THE_ANSWER ) );
 
 			return trackedPromise.then( data => {
