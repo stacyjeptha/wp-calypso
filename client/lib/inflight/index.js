cc82e1fe53110b2eaf55f87db6debd59e71c4d8c
Jake F - @samouri <samouri@users.noreply.github.com>
2017-01-31 11:00:40 -0500

    new func to inflight

--- client/lib/inflight/index.js
+++ client/lib/inflight/index.js
@@ -1,24 +1,30 @@
 const inflight = new Set();
 
 // Utility methods to track inflight async requests
-export default {
-	requestInflight: function( requestKey ) {
-		return inflight.has( requestKey );
-	},
-
-	startRequest: function( requestKey ) {
-		inflight.add( requestKey );
-	},
-
-	endRequest: function( requestKey ) {
-		inflight.delete( requestKey );
-	},
-
-	requestTracker: function( requestKey, callback ) {
-		inflight.add( requestKey );
-		return function( error, data ) {
-			inflight.delete( requestKey );
-			callback( error, data );
-		};
-	},
+export function requestInflight( requestKey ) {
+	return inflight.has( requestKey );
 };
+
+export function startRequest( requestKey ) {
+	inflight.add( requestKey );
+}
+
+export function endRequest( requestKey ) {
+	inflight.delete( requestKey );
+}
+
+export function requestTracker( requestKey, callback ) {
+	inflight.add( requestKey );
+	return function( error, data ) {
+		inflight.delete( requestKey );
+		callback( error, data );
+	};
+}
+
+export function dedupedRequest( requestKey, callback ) {
+	if ( inflight.has( requestKey ) ) {
+		return;
+	}
+
+	return requestTracker( requestKey, callback );
+}
