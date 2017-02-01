fb1a862455f452332780644d2c78eede55348b69
budzanowski <bartosz.budzanowski@gmail.com>
2017-02-01 16:01:44 +0100

    Experiment a bit.

--- client/my-sites/themes/theme-options.js
+++ client/my-sites/themes/theme-options.js
@@ -17,6 +17,7 @@ import {
 	confirmDelete,
 } from 'state/themes/actions';
 import {
+	getTheme,
 	getThemeSignupUrl as getSignupUrl,
 	getThemePurchaseUrl as getPurchaseUrl,
 	getThemeCustomizeUrl as	getCustomizeUrl,
@@ -32,6 +33,8 @@ import { isJetpackSite } from 'state/sites/selectors';
 import { hasFeature } from 'state/sites/plans/selectors';
 import { canCurrentUser } from 'state/selectors';
 import { FEATURE_UNLIMITED_PREMIUM_THEMES } from 'lib/plans/constants';
+import { setLayoutFocus } from 'state/ui/layout-focus/actions';
+import { setPreviewUrl, setPreviewType } from 'state/ui/preview/actions';
 
 const purchase = config.isEnabled( 'upgrades/checkout' )
 	? {
@@ -117,12 +120,22 @@ const tryAndCustomizeOnJetpack = {
 	)
 };
 
+function requestPreview( themeId ) {
+	return ( dispatch, getState ) => {
+		const { demo_uri: uri } = getTheme( getState(), 'wpcom', themeId );
+		dispatch( setPreviewUrl( uri ) );
+		dispatch( setPreviewType( 'site-preview' ) );
+		dispatch( setLayoutFocus( 'preview' ) );
+	};
+}
+
 // This is a special option that gets its `action` added by `ThemeShowcase` or `ThemeSheet`,
 // respectively. TODO: Replace with a real action once we're able to use `SitePreview`.
 const preview = {
 	label: i18n.translate( 'Live demo', {
 		comment: 'label for previewing the theme demo website'
 	} ),
+	action: ( themeId, siteId, theme ) => requestPreview( themeId, siteId, theme ),
 	hideForTheme: ( state, theme, siteId ) => isActive( state, theme.id, siteId )
 };
 
