3794f21f1f1d91454cde0441894717a24e2564e7
Copons <copons@users.noreply.github.com>
2017-02-01 11:48:56 +0100

    Slow committing to avoid killing git history 4

--- client/my-sites/posts/post-controls/index.jsx
+++ client/my-sites/posts/post-controls/index.jsx
@@ -205,6 +205,156 @@ export class PostControls extends Component {
 
 	onHideMore = () => this.setState( { showMoreOptions: false } );
 
+	getAvailableControls() {
+		const {
+			canUserDeletePost,
+			canUserEditPost,
+			canUserPublishPost,
+			editURL,
+			fullWidth,
+			onDelete,
+			onPublish,
+			onRestore,
+			onToggleShare,
+			onTrash,
+			post,
+			recordCopyPost,
+			recordEditPost,
+			recordPreviewPost,
+			recordViewPost,
+			recordViewPostStats,
+			site,
+			translate,
+		} = this.props;
+		const controls = { main: [], more: [] };
+
+		// NOTE: Currently Jetpack site posts are not returning `post.capabilities`
+		// and those posts will not have access to post management type controls
+
+		// Main Controls (not behind ... more link)
+		if ( canUserEditPost ) {
+			controls.main.push( {
+				className: 'edit',
+				href: editURL,
+				icon: 'pencil',
+				onClick: recordEditPost,
+				text: translate( 'Edit' ),
+			} );
+		}
+
+		if ( 'publish' === post.status ) {
+			controls.main.push( {
+				className: 'view',
+				href: post.URL,
+				icon: 'external',
+				onClick: recordViewPost,
+				target: '_blank',
+				text: translate( 'View' ),
+			} );
+
+			controls.main.push( {
+				className: 'stats',
+				href: `/stats/post/${ post.ID }/${ site.slug }`,
+				icon: 'stats-alt',
+				onClick: recordViewPostStats,
+				text: translate( 'Stats' ),
+			} );
+
+			if ( isEnabled( 'republicize' ) ) {
+				controls.main.push( {
+					className: 'share',
+					disabled: ! this.props.isPublicizeEnabled,
+					icon: 'share',
+					onClick: onToggleShare,
+					text: translate( 'Share' ),
+				} );
+			}
+		} else if ( 'trash' !== post.status ) {
+			const parsedUrl = url.parse( post.URL, true );
+			parsedUrl.query.preview = true;
+			// NOTE: search needs to be cleared in order to rebuild query
+			// http://nodejs.org/api/url.html#url_url_format_urlobj
+			parsedUrl.search = '';
+
+			controls.main.push( {
+				className: 'view',
+				href: url.format( parsedUrl ),
+				icon: 'external',
+				onClick: recordPreviewPost,
+				text: translate( 'Preview' ),
+			} );
+
+			if ( canUserPublishPost ) {
+				controls.main.push( {
+					className: 'publish',
+					icon: 'reader',
+					onClick: onPublish,
+					text: translate( 'Publish' ),
+				} );
+			}
+		} else if ( canUserDeletePost ) {
+			controls.main.push( {
+				className: 'restore',
+				icon: 'undo',
+				onClick: onRestore,
+				text: translate( 'Restore' ),
+			} );
+		}
+
+		if ( canUserDeletePost ) {
+			if ( 'trash' === post.status ) {
+				controls.main.push( {
+					className: 'trash is-scary',
+					icon: 'trash',
+					onClick: onDelete,
+					text: translate( 'Delete Permanently' ),
+				} );
+			} else {
+				controls.main.push( {
+					className: 'trash',
+					icon: 'trash',
+					onClick: onTrash,
+					text: translate( 'Trash' ),
+				} );
+			}
+		}
+
+		if ( ( 'publish' === post.status || 'private' === post.status ) && canUserEditPost ) {
+			controls.main.push( {
+				className: 'copy',
+				href: `/post/${ site.slug }?copy=${ post.ID }`,
+				icon: 'clipboard',
+				onClick: recordCopyPost,
+				text: translate( 'Copy' ),
+			} );
+		}
+
+		// More Controls (behind ... more link)
+		if ( controls.main.length > 4 && fullWidth ) {
+			controls.more = controls.main.splice( 4 );
+		} else if ( controls.main.length > 2 && ! fullWidth ) {
+			controls.more = controls.main.splice( 2 );
+		}
+
+		if ( controls.more.length ) {
+			controls.main.push( {
+				className: 'more',
+				icon: 'ellipsis',
+				onClick: this.onShowMore,
+				text: translate( 'More' ),
+			} );
+
+			controls.more.push( {
+				className: 'back',
+				icon: 'chevron-left',
+				onClick: this.onHideMore,
+				text: translate( 'Back' ),
+			} );
+		}
+
+		return controls;
+	}
+
 	render() {
 		const { main, more } = getAvailableControls();
 		const classes = classNames( 'post-controls', {
