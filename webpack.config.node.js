a14ef2d5415d31390ad092392117f68cd0d20c55
Tugdual de Kerviler <dekervit@gmail.com>
2017-02-01 16:25:40 +0100

    Do not load the style loader twice Just replace it with the isomorphice style loader

--- webpack.config.node.js
+++ webpack.config.node.js
@@ -89,8 +89,7 @@ var webpackConfig = {
 				test: /\.scss$/,
 				loaders: [
 					'isomorphic-style-loader',
-					'style-loader',
-					'css-loader?modules&camelCase&importLoaders=1&localIdentName=[path][local]',
+					'css-loader?modules&camelCase=dashes&importLoaders=1&localIdentName=[path][local]',
 					'sass-loader'
 				]
 			}
